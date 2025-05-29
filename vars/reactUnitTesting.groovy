da52b6eb65d684e5444284239dd8ff353e3b5e9d
Himanshu Parashar <himanshuparashar085@gmail.com>
2025-05-29 14:22:58 +0530

    Update reactUnitTesting.groovy

--- vars/reactUnitTesting.groovy
+++ vars/reactUnitTesting.groovy
@@ -1,5 +1,8 @@
 def call() {
     dir('frontend') {
+        // Install project dependencies (this includes react-scripts)
+        sh 'npm install'
+
         // Run unit tests with coverage
         sh 'npm test -- --coverage --watchAll=false'
     }
