f2c72e10a46fa6f6f0f8d2abb8ddf15f90699c5e
Himanshu Parashar <himanshuparashar085@gmail.com>
2025-05-29 14:21:39 +0530

    Update reactUnitTesting.groovy

--- vars/reactUnitTesting.groovy
+++ vars/reactUnitTesting.groovy
@@ -1,15 +1,6 @@
 def call() {
     dir('frontend') {
-        // Install dependencies including testing libraries
-        sh '''
-            npm install
-            npm install --save-dev @testing-library/react@12.1.5 @testing-library/jest-dom@5.16.5
-        '''
-
-        // Run unit tests without watch mode
-        sh 'npm test -- --watchAll=false'
-
-        // Run tests again to generate coverage report without watch mode
+        // Run unit tests with coverage
         sh 'npm test -- --coverage --watchAll=false'
     }
 }
